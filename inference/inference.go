9f07127d40bc98120eac8e4e1a37da80f8754593
FelixJobson <felix.jobson@tiki-institut.com>
2023-07-02 12:45:58 +0200

    compute softmax

--- inference/inference.go
+++ inference/inference.go
@@ -3,6 +3,7 @@ package inference
 import (
 	"io/ioutil"
 	"log"
+	"math"
 
 	"github.com/owulveryck/onnx-go"
 	"github.com/owulveryck/onnx-go/backend/x/gorgonnx"
@@ -31,5 +32,19 @@ func Infere(model_name string, input *tensor.Dense) []float32 {
 	logits_raw, _ := model.GetOutputTensors()
 	logits := logits_raw[0].Data().([]float32)
 
-	return logits
+	return softmax(logits)
+}
+
+func softmax(input []float32) []float32 {
+	var sum float32
+	for i := range input {
+		input[i] = float32(math.Exp(float64(input[i])))
+		sum += input[i]
+	}
+
+	for i := range input {
+		input[i] /= sum
+	}
+
+	return input
 }
